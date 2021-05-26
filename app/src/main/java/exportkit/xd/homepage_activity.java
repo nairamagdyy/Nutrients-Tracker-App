031c132b7d17d128682f3f32407c9591dbe08694
nairamagdyy <36415970+nairamagdyy@users.noreply.github.com>
2021-05-26 04:46:49 +0200

    Initial commit

---
+++ app/src/main/java/exportkit/xd/homepage_activity.java
@@ -0,0 +1,117 @@
+
+
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc
+	 *	@file 		untitled
+	 *	@date 		1621375453134
+	 *	@title 		Page 1
+	 *	@author
+	 *	@keywords
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+
+
+	package exportkit.xd;
+
+	import android.app.Activity;
+	import android.os.Bundle;
+
+
+	import android.view.View;
+	import android.widget.TextView;
+	import android.widget.ImageView;
+	import android.content.Intent;
+
+	public class homepage_activity extends Activity {
+
+
+		private View _bg__homepage_ek2;
+		private View rectangle_2_16_ek3;
+		private View ellipse_ek6;
+		private View ellipse_ek7;
+		private TextView home;
+		private TextView hello_taylor_;
+		private View rectangle;
+		private View ellipse_ek8;
+		private View rectangle_2;
+		private View rectangle_3;
+		private View rectangle_3_1;
+		private ImageView union;
+		private View rectangle_2_1;
+		private TextView home_ek1;
+		private ImageView vector_ek8;
+		private ImageView _profile;
+		private TextView let_s_learn_more_about_your_food;
+		private TextView search_;
+		private View rectangle_2_18;
+		private ImageView foodcam_1;
+		private ImageView overlay_ek1;
+		private TextView track_nutrients_foreach_component_in_your_food;
+		private TextView food_camera;
+		private ImageView vector_ek9;
+		private ImageView notification_ek1;
+		private View rectangle_2_18_ek1;
+		private ImageView foodcam_1_ek1;
+		private ImageView overlay_ek2;
+		private TextView share_your_own_recipe_and_its_nutrients_facts_with_your_followers;
+		private TextView add_recipe;
+		private ImageView vector_ek10;
+		private TextView _9_41;
+		private View rectangle_ek1;
+		private View rectangle_ek2;
+		private View rectangle_ek3;
+		private View rectangle_ek4;
+		private View rectangle_ek5;
+		private View rectangle_ek6;
+		private ImageView vector_2;
+		private ImageView vector_ek11;
+		private ImageView vector_ek12;
+		private ImageView vector_ek13;
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.homepage);
+
+
+			_bg__homepage_ek2 = (View) findViewById(R.id._bg__homepage_ek2);
+			rectangle_2_16_ek3 = (View) findViewById(R.id.rectangle_2_16_ek3);
+			ellipse_ek6 = (View) findViewById(R.id.ellipse_ek6);
+			ellipse_ek7 = (View) findViewById(R.id.ellipse_ek7);
+			home = (TextView) findViewById(R.id.home);
+			hello_taylor_ = (TextView) findViewById(R.id.hello_taylor_);
+			rectangle = (View) findViewById(R.id.rectangle);
+			ellipse_ek8 = (View) findViewById(R.id.ellipse_ek8);
+			rectangle_2 = (View) findViewById(R.id.rectangle_2);
+			rectangle_3 = (View) findViewById(R.id.rectangle_3);
+			rectangle_3_1 = (View) findViewById(R.id.rectangle_3_1);
+			union = (ImageView) findViewById(R.id.union);
+			home_ek1 = (TextView) findViewById(R.id.home_ek1);
+			vector_ek8 = (ImageView) findViewById(R.id.vector_ek8);
+			_profile = (ImageView) findViewById(R.id._profile);
+			let_s_learn_more_about_your_food = (TextView) findViewById(R.id.let_s_learn_more_about_your_food);
+			search_ = (TextView) findViewById(R.id.search_);
+			rectangle_2_18 = (View) findViewById(R.id.rectangle_2_18);
+			foodcam_1 = (ImageView) findViewById(R.id.foodcam_1);
+			overlay_ek1 = (ImageView) findViewById(R.id.overlay_ek1);
+			track_nutrients_foreach_component_in_your_food = (TextView) findViewById(R.id.track_nutrients_foreach_component_in_your_food);
+			food_camera = (TextView) findViewById(R.id.food_camera);
+			rectangle_2_18_ek1 = (View) findViewById(R.id.rectangle_2_18_ek1);
+			foodcam_1_ek1 = (ImageView) findViewById(R.id.foodcam_1_ek1);
+			overlay_ek2 = (ImageView) findViewById(R.id.overlay_ek2);
+			share_your_own_recipe_and_its_nutrients_facts_with_your_followers = (TextView) findViewById(R.id.share_your_own_recipe_and_its_nutrients_facts_with_your_followers);
+			add_recipe = (TextView) findViewById(R.id.add_recipe);
+
+
+
+			//custom code goes here
+
+		}
+	}
+	
+	
---
+++ app/src/main/java/exportkit/xd/sign_up_activity.java
@@ -0,0 +1,104 @@
+	package exportkit.xd;
+
+	import android.app.Activity;
+	import android.content.Intent;
+	import android.os.Bundle;
+
+
+	import android.view.View;
+	import android.widget.TextView;
+	import android.widget.ImageView;
+
+	public class sign_up_activity extends Activity {
+
+
+		private View _bg__sign_up_ek5;
+		private View rectangle_33_ek2;
+		private TextView phone_number;
+		private ImageView download_1;
+		private View rectangle_33_ek3;
+		private TextView name;
+		private ImageView gs1_user_grey_1;
+		private View rectangle_33_ek4;
+		private TextView username;
+		private ImageView gs1_user_grey_1_ek1;
+		private TextView sign_up_ek6;
+		private View rectangle_33_ek5;
+		private TextView email_address;
+		private ImageView path_445_ek1;
+		private ImageView rectangle_511_ek1;
+		private View rectangle_33_ek6;
+		private ImageView stroke_1_ek2;
+		private ImageView stroke_3_ek2;
+		private TextView password_ek1;
+		private ImageView stroke_1_ek3;
+		private ImageView stroke_3_ek3;
+		private ImageView stroke_5_ek1;
+		private View male;
+		private View female;
+		private TextView female_ek1;
+		private TextView male_ek1;
+		private View home_indicator_ek2;
+		private View rectangle_2_16_ek1;
+		private View ellipse_ek2;
+		private TextView welcome_;
+		private View ellipse_ek3;
+		private TextView sign_up_ek7;
+		private ImageView vector_ek4;
+		private ImageView vector_ek5;
+		private View border_ek2;
+		private ImageView cap_ek2;
+		private View capacity_ek2;
+		private ImageView wifi_ek2;
+		private ImageView cellular_connection_ek2;
+		private TextView time_ek2;
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.sign_up);
+
+			rectangle_33_ek2 = (View) findViewById(R.id.rectangle_33_ek2);
+			phone_number = (TextView) findViewById(R.id.phonenumber);
+			download_1 = (ImageView) findViewById(R.id.download_1);
+			rectangle_33_ek3 = (View) findViewById(R.id.rectangle_33_ek3);
+			name = (TextView) findViewById(R.id.name);
+			gs1_user_grey_1 = (ImageView) findViewById(R.id.gs1_user_grey_1);
+			rectangle_33_ek4 = (View) findViewById(R.id.rectangle_33_ek4);
+			username = (TextView) findViewById(R.id.username);
+			gs1_user_grey_1_ek1 = (ImageView) findViewById(R.id.gs1_user_grey_1_ek1);
+			sign_up_ek6 = (TextView) findViewById(R.id.sign_up_ek6);
+			rectangle_33_ek5 = (View) findViewById(R.id.rectangle_33_ek5);
+			email_address = (TextView) findViewById(R.id.email_address);
+			path_445_ek1 = (ImageView) findViewById(R.id.path_445_ek1);
+			rectangle_511_ek1 = (ImageView) findViewById(R.id.rectangle_511_ek1);
+			rectangle_33_ek6 = (View) findViewById(R.id.rectangle_33_ek6);
+			password_ek1 = (TextView) findViewById(R.id.password_ek1);
+			stroke_1_ek3 = (ImageView) findViewById(R.id.stroke_1_ek3);
+			stroke_3_ek3 = (ImageView) findViewById(R.id.stroke_3_ek3);
+			stroke_5_ek1 = (ImageView) findViewById(R.id.stroke_5_ek1);
+			male = (View) findViewById(R.id.male);
+			female = (View) findViewById(R.id.female);
+			rectangle_2_16_ek1 = (View) findViewById(R.id.rectangle_2_16_ek1);
+			ellipse_ek2 = (View) findViewById(R.id.ellipse_ek2);
+			welcome_ = (TextView) findViewById(R.id.welcome_);
+			ellipse_ek3 = (View) findViewById(R.id.ellipse_ek3);
+			sign_up_ek7 = (TextView) findViewById(R.id.sign_up_ek7);
+
+			//custom code goes here
+			sign_up_ek6.setOnClickListener(new View.OnClickListener() {
+
+				public void onClick(View v) {
+
+					Intent nextScreen = new Intent(getApplicationContext(), homepage_activity.class);
+					startActivity(nextScreen);
+
+
+				}
+			});
+
+		}
+	}
+	
+	
