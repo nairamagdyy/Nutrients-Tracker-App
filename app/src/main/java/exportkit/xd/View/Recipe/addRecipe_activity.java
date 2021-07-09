868894cdaf31faade389cfba59a8e0b577b36137
Nada Mahmoud <36458601+nadamahmoudd@users.noreply.github.com>
2021-07-09 22:38:34 +0200

    create session

---
+++ app/src/main/java/exportkit/xd/View/Recipe/addRecipe_activity.java
@@ -0,0 +1,37 @@
+package exportkit.xd.View.Recipe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+import exportkit.xd.Controller.IRecipeController;
+import exportkit.xd.Controller.recipeController;
+
+import exportkit.xd.R;
+import exportkit.xd.View.macroTracker_activity;
+
+public class addRecipe_activity extends Activity  implements IAddRecipeView {
+    IRecipeController RecipeController;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_recipe);
+        RecipeController = new recipeController(this);
+    }
+
+    @Override
+    public void onAddSuccess(String message) {
+        Toast.makeText(getApplication(),message,Toast.LENGTH_LONG).show();
+        Intent nextScreen = new Intent(getApplicationContext(), macroTracker_activity.class);
+        startActivity(nextScreen);
+    }
+
+    @Override
+    public void onAddError(String message) {
+        Toast.makeText(getApplication(), message, Toast.LENGTH_LONG).show();
+
+    }
+}
---
+++ app/src/main/java/exportkit/xd/View/macroTracker_activity.java
@@ -0,0 +1,6 @@
+package exportkit.xd.View;
+
+import android.app.Activity;
+
+public class macroTracker_activity extends Activity {
+}
