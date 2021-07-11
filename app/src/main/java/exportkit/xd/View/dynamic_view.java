002bdeaa66f55b679475d4cfc70fedf25f7b2ef3
nairamagdyy <36415970+nairamagdyy@users.noreply.github.com>
2021-07-11 16:09:42 +0200

    dynamic view

--- app/src/main/java/exportkit/xd/View/dynamic_view.java
+++ app/src/main/java/exportkit/xd/View/dynamic_view.java
@@ -3,37 +3,46 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import exportkit.xd.R;
 
-public class dynamic_view extends Activity implements OnClickListener{
+public class dynamic_view extends AppCompatActivity implements OnClickListener {
 
-
+       LinearLayout newlayer,newstep;
+       Button buttonAdd,step;
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.addrecipe_one);
-            View buttonAdd = findViewById(R.id.ingredient);
+            buttonAdd = findViewById(R.id.ingredient);
+            newlayer = findViewById(R.id.linearLayout1);
+            step = findViewById(R.id.step);
+            newstep = findViewById(R.id.addstep);
             buttonAdd.setOnClickListener(this);
+            step.setOnClickListener(this);
         }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ingredient:
+                addingredient();
 
-        public void onClick(View v) {
-            switch (v.getId()) {
-
-                case R.id.ingredient:
-                    //Check if the Layout already exists
-                    LinearLayout hiddenLayout = (LinearLayout)findViewById(R.id.hiddenLayout);
-                    if(hiddenLayout == null){
-                        //Inflate the Hidden Layout Information View
-                        LinearLayout myLayout = (LinearLayout)findViewById(R.id.linearLayout2);
-                        View hiddenInfo = getLayoutInflater().inflate(R.layout.hidden, myLayout, false);
-                        myLayout.addView(hiddenInfo);
-                    }
-                    break;
-                    
+            case R.id.step:
+                addstep();
+        }
+    }
+            public void   addingredient(){
+            View view= getLayoutInflater().inflate(R.layout.hidden,null);
+            newlayer.addView(view);
             }
-
-        }
+    public void   addstep(){
+        View view= getLayoutInflater().inflate(R.layout.hiddenstep,null);
+        newstep.addView(view);
+    }
 
 }
