ec3a801bd778f862eafeb1728a4a98975d4c5405
ALaa Farouk <alaafar19@gmail.com>
2021-07-11 19:37:56 +0200

    Some Changes

--- app/src/main/java/exportkit/xd/View/Register/log_in_activity.java
+++ app/src/main/java/exportkit/xd/View/Register/log_in_activity.java
@@ -16,6 +16,7 @@ import exportkit.xd.Controller.IUserController;
 import exportkit.xd.Controller.userController;
 import exportkit.xd.DB.AppDBController;
 import exportkit.xd.R;
+import exportkit.xd.View.Profile.Editprofile_Activity;
 import exportkit.xd.View.Recipe.addRecipe_activity;
 import exportkit.xd.View.homepage_activity;
 
@@ -92,7 +93,7 @@ public class log_in_activity extends Activity implements IRegisterView {
 	@Override
 	public void onLoginSuccess(String message) {
 		Toast.makeText(getApplication(),message,Toast.LENGTH_LONG).show();
-		Intent nextScreen = new Intent(getApplicationContext(), addRecipe_activity.class);
+		Intent nextScreen = new Intent(getApplicationContext(), Editprofile_Activity.class);
 		startActivity(nextScreen);
 	}
 
