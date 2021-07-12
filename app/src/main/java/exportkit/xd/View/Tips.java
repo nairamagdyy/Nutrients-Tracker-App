43e79bb84e4f461d574c6a88329ee535582e22fc
nairamagdyy <36415970+nairamagdyy@users.noreply.github.com>
2021-07-12 17:02:15 +0200

    Tips

---
+++ app/src/main/java/exportkit/xd/View/Tips.java
@@ -0,0 +1,77 @@
+package exportkit.xd.View;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import exportkit.xd.R;
+import exportkit.xd.View.Profile.myProfile_activity;
+
+public class Tips extends AppCompatActivity {
+
+    Button increase_fats,increase_carbs,increase_proteins, search;
+    ImageButton home, profile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.macro_tracker);
+        search = findViewById(R.id.search1);
+        home = (ImageButton)findViewById(R.id.home1);
+        profile = (ImageButton)findViewById(R.id.profile1);
+        increase_fats = findViewById(R.id.increasefats);
+        increase_proteins = findViewById(R.id.increaseproteins);
+        increase_fats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fats();
+            }
+        });
+        search.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent nextScreen = new Intent(getApplicationContext(), searchUser_activity.class);
+                startActivity(nextScreen);
+
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent nextScreen = new Intent(getApplicationContext(), homepage_activity.class);
+                startActivity(nextScreen);
+
+            }
+        });
+        profile.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent nextScreen = new Intent(getApplicationContext(), myProfile_activity.class);
+                startActivity(nextScreen);
+
+            }
+        });
+    }
+    /*Display Tips*/
+    private void fats() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Tips");
+        dialogBuilder.setMessage("Some informative message for the user to do that.");
+        dialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialogBuilder.create().show();
+    }
+}
