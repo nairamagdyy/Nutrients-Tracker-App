ebd42af93241fba111a8a294a29fa0fb01ac0612
nairamagdyy <36415970+nairamagdyy@users.noreply.github.com>
2021-07-13 01:30:33 +0200

    adapter

---
+++ app/src/main/java/exportkit/xd/View/adapter.java
@@ -0,0 +1,66 @@
+package exportkit.xd.View;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import exportkit.xd.R;
+
+public class adapter extends RecyclerView.Adapter<adapter.ViewHolder> {
+
+    List<String> titles;
+    List<Integer> images;
+    LayoutInflater inflater;
+
+    public adapter(Context ctx, List<String> titles, List<Integer> images){
+        this.titles = titles;
+        this.images = images;
+        this.inflater = LayoutInflater.from(ctx);
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.recipe,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.title.setText(titles.get(position));
+        holder.gridIcon.setImageResource(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        ImageView gridIcon;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.textView2);
+            gridIcon = itemView.findViewById(R.id.imageView2);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext(), "Clicked -> " + getAdapterPosition(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
