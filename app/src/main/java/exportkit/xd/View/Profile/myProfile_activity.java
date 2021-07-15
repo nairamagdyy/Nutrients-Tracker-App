f84247f10cf67b3083c8d0d4929c15318499dbf0
Nada Mahmoud <36458601+nadamahmoudd@users.noreply.github.com>
2021-07-15 17:28:42 +0200

    view favorite list

--- app/src/main/java/exportkit/xd/View/Profile/myProfile_activity.java
+++ app/src/main/java/exportkit/xd/View/Profile/myProfile_activity.java
@@ -1,4 +1,5 @@
 package exportkit.xd.View.Profile;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -35,7 +36,7 @@ public  class myProfile_activity extends Activity implements IProfile, IAppViews
     private CircularImageView uploadedImage, ProfileIcon;
     private TextView name , username ;
     private ImageButton HomeButton, editButton , logoutBtn ;
-    private Button FavButton , SearchButton;
+    private Button FavButton, SearchButton, RecipeButton;
 
     userController UserController;
     recipeController RecipeController;
@@ -60,6 +61,7 @@ public  class myProfile_activity extends Activity implements IProfile, IAppViews
         FavButton = (Button) findViewById(R.id.FavoriteButton) ;
         editButton = (ImageButton) findViewById(R.id.edit11) ;
         logoutBtn= (ImageButton) findViewById(R.id._list_1) ;
+        RecipeButton= (Button)findViewById(R.id.recipes);
 
         SearchButton = (Button) findViewById(R.id.ellipse_ek22);
         HomeButton = (ImageButton) findViewById(R.id.home_ek11);
@@ -113,10 +115,26 @@ public  class myProfile_activity extends Activity implements IProfile, IAppViews
                 startActivity(nextScreen);
             }
         });
-        FavButton.setOnClickListener(new View.OnClickListener() {
+        RecipeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent nextScreen = new Intent(getApplicationContext(), myFavorites_activity.class);
+                Intent nextScreen = new Intent(getApplicationContext(), myProfile_activity.class);
                 startActivity(nextScreen);
+            }
+        });
+        FavButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            public void onClick(View v) {
+                RecipeButton.setTextColor(R.color.choice);
+                FavButton.setTextColor(R.color.select);
+
+                //Recyclerview as GridView for recipes
+                //get user recipe from db
+                Vector<Integer> favRecipesIdList= RecipeController.viewFavList((int) loggedUser);
+                for(int i=0; i<favRecipesIdList.size();i++){
+                    Recipe fav= RecipeController.getRecipe(favRecipesIdList.get(i));
+                    recipeNameList.add(fav.getName());
+                    recipeImageList.add(fav.getImage());
+                }
             }
         });
         SearchButton.setOnClickListener(new View.OnClickListener() {
