09486c5c0499cc538a8f2bbb35bd28a8f4d52d16
Nada Mahmoud <36458601+nadamahmoudd@users.noreply.github.com>
2021-07-24 19:46:00 +0200

    .

--- app/src/main/java/exportkit/xd/View/Scanner/Scanner_activity.java
+++ app/src/main/java/exportkit/xd/View/Scanner/Scanner_activity.java
@@ -1,7 +1,6 @@
 package exportkit.xd.View.Scanner;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
