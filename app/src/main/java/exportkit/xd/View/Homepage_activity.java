5f213c348c8b4ba9277da6efc73e3cd5e340b5e9
Nada Mahmoud <36458601+nadamahmoudd@users.noreply.github.com>
2021-07-25 04:23:37 +0200

    session

--- app/src/main/java/exportkit/xd/View/Homepage_activity.java
+++ app/src/main/java/exportkit/xd/View/Homepage_activity.java
@@ -33,51 +33,58 @@ public class Homepage_activity extends Activity implements IAppViews{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.homepage);
 
-		userController = new UserController(this);
-
-		name = (TextView) findViewById(R.id.hello);
-		addRecipeButton = (ImageButton) findViewById(R.id.addrecipebutton);
-		cameraButton= findViewById(R.id.openCamera);
-		ProfileButton = findViewById(R.id.profile1);
-		SearchButton = (Button) findViewById(R.id.ellipse_ek22);
-
-
 		SessionManager session= new SessionManager(this);
 		long loggedUserID= session.getUserFromSession();
-		User user= userController.getUser((int)loggedUserID);
+		if(loggedUserID>0) {
 
-		//set current information for logged user
-		if(user.getAvatar() != null){
-			ProfileButton.setImageURI(Uri.parse(user.getAvatar()));
+			userController = new UserController(this);
+			User user = userController.getUser((int) loggedUserID);
+
+
+			name = (TextView) findViewById(R.id.hello);
+			addRecipeButton = (ImageButton) findViewById(R.id.addrecipebutton);
+			cameraButton = findViewById(R.id.openCamera);
+			ProfileButton = findViewById(R.id.profile1);
+			SearchButton = (Button) findViewById(R.id.ellipse_ek22);
+
+
+			//set current information for logged user
+			if (user.getAvatar() != null) {
+				ProfileButton.setImageURI(Uri.parse(user.getAvatar()));
+			}
+			name.setText("Hello " + user.getName());
+
+			ProfileButton.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					Intent nextScreen = new Intent(getApplicationContext(), Profile_activity.class);
+					startActivity(nextScreen);
+				}
+			});
+			SearchButton.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					Intent nextScreen = new Intent(getApplicationContext(), SearchUser_activity.class);
+					startActivity(nextScreen);
+					Toast.makeText(getApplication(), "Now You are On Search User Mode", Toast.LENGTH_LONG).show();
+
+				}
+			});
+			addRecipeButton.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					Intent nextScreen = new Intent(getApplicationContext(), AddRecipe_activity.class);
+					startActivity(nextScreen);
+				}
+			});
+			cameraButton.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					//Intent nextScreen = new Intent(getApplicationContext(), .class);
+					//startActivity(nextScreen);
+				}
+			});
 		}
-		name.setText("Hello " + user.getName());
-
-		ProfileButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent nextScreen = new Intent(getApplicationContext(), Profile_activity.class);
-				startActivity(nextScreen);
-			}
-		});
-		SearchButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent nextScreen = new Intent(getApplicationContext(), SearchUser_activity.class);
-				startActivity(nextScreen);
-				Toast.makeText(getApplication(),"Now You are On Search User Mode",Toast.LENGTH_LONG).show();
-
-			}
-		});
-		addRecipeButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent nextScreen = new Intent(getApplicationContext(), AddRecipe_activity.class);
-				startActivity(nextScreen);
-			}
-		});
-		cameraButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v){
-				//Intent nextScreen = new Intent(getApplicationContext(), .class);
-				//startActivity(nextScreen);
-			}
-		});
+		else{
+			Intent nextScreen = new Intent(getApplicationContext(), CoverPage_activity.class);
+			startActivity(nextScreen);
+		}
 	}
 
 	@Override
