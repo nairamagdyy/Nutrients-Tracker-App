13e3b98f98a380b00b042b1d1bb94d940c43ad3a
ALaa Farouk <alaafar19@gmail.com>
2021-07-25 05:33:46 +0200

    :)))))

--- app/src/main/java/exportkit/xd/View/Homepage_activity.java
+++ app/src/main/java/exportkit/xd/View/Homepage_activity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.blogspot.atifsoftwares.circularimageview.CircularImageView;
+import org.opencv.android.OpenCVLoader;
 
 import exportkit.xd.Controller.UserController;
 import exportkit.xd.DB.SessionManager;
@@ -18,6 +20,7 @@ import exportkit.xd.Model.User;
 import exportkit.xd.R;
 import exportkit.xd.View.Profile.Profile_activity;
 import exportkit.xd.View.Recipe.AddRecipe_activity;
+import exportkit.xd.View.Scanner.ScannerActivity;
 import exportkit.xd.View.Search.SearchUser_activity;
 
 public class Homepage_activity extends Activity implements IAppViews{
@@ -27,6 +30,14 @@ public class Homepage_activity extends Activity implements IAppViews{
 	private Button SearchButton;
 
 	UserController userController;
+	static {
+		if(OpenCVLoader.initDebug()){
+			Log.d("Homepage_activity : ","Opencv is loaded");
+		}
+		else {
+			Log.d("Homepage_activity : ","Opencv failed to load");
+		}
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -76,8 +87,8 @@ public class Homepage_activity extends Activity implements IAppViews{
 			});
 			cameraButton.setOnClickListener(new View.OnClickListener() {
 				public void onClick(View v) {
-					//Intent nextScreen = new Intent(getApplicationContext(), .class);
-					//startActivity(nextScreen);
+					startActivity(new Intent(getApplication(), ScannerActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+
 				}
 			});
 		}
